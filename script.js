c4feaab2e8daead26bb3bde362e2b2233d907c77
OK946 <owen.kolbert24@student.cart.org>
2025-05-20 14:49:16 -0700

    Calculator

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const c = document.getElementById("canvas");
-const ctx = c.getContext("2d");
+const calculate = document.getElementById(`calculateButton`);
+const input = document.getElementById(`calculatorInput`);
+const output = document.getElementById(`equation`);
+const history = document.getElementById(`history`);
+let historyList = ``;
+
+calculate.addEventListener(`click`,() =>{
+    if(input.value.length !== 0){
+        if(output.innerHtml !== `<p>Type to Begin</p>`){
+            historyList += `<div class="answer"><p>${input.value}</p><p>5</p></div>`
+            history.innerHTML = historyList;
+        }
+        output.innerHTML = `<div class="answer"><p>${input.value}</p><p>${5 + 4}</p></div>`
+    }
+});
